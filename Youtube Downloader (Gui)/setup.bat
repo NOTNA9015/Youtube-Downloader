514f037941e6f09341a3dd6a39efdc9ff0dbd6ba
NOTNA9015 <162547923+NOTNA9015@users.noreply.github.com>
2025-05-23 12:56:50 +0200

    Update setup.bat

--- Youtube Downloader (Gui)/setup.bat
+++ Youtube Downloader (Gui)/setup.bat
@@ -1,7 +1,7 @@
 @echo off
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-title Youtube Downloader (setup)
+title Youtube Downloader (Setup)
 SETLOCAL EnableDelayedExpansion
 REM Check if Python is installed
 py --version >nul 2>&1
